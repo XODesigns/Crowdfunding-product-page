32e8e705b048fa0c1de3d8f20f5ee22c39064de8
XODesigns <xolelanimdima@gmail.com>
2022-04-21 22:29:30 +0200

    upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
+const backUs = document.querySelector(".back-us")
+const modalMain = document.querySelector(".modal")
 
-$(".back-us").click(function(){
-    $(".modal").removeClass("visibility")
+backUs.addEventListener("click", function(){
+    modalMain.classList.remove("visibility")
 })
+
+
+// $(".back-us").click(function(){
+//     $(".modal").removeClass("visibility")
+// })
