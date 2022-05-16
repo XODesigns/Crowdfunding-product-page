aa757336e9f279d229ed16ab8b39fd934cb0305e
XODesigns <xolelanimdima@gmail.com>
2022-05-16 20:31:00 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
 const backUs = document.querySelector(".back-us")
 const modalMain = document.querySelector(".modal")
-const pledgeSect = document.querySelectorAll("label")
-const visible = document.querySelectorAll(".pledge")
+const check = document.querySelectorAll("#checkbox")
 
 backUs.addEventListener("click", ()=>{
     modalMain.classList.remove("visibility")
 })
 
 
+/** Modal checkbox event in jquery -  to togglen visibility for the pledge section */
 
-pledgeSect.forEach((e)=>{
-    e.addEventListener("click", ()=>{
-for(let i = 0; i<visible;i++){
-    visible[i].classList.remove("hide")
-}
-  
-})
-        
+
+    $('.first').click(()=>{
+        if($('#first').prop("checked") == true){
+            $('.one').removeClass('hide');
+        }
+        else if($('#first').prop("checked") == false){
+            $('.one').addClass('hide');
+        }
+    })
+
+    $('.second').click(()=>{
+        if($('#second').prop("checked") == true){
+            $('.two').removeClass('hide');
+        }
+        else if($('#second').prop("checked") == false){
+            $('.two').addClass('hide');
+        }
+    })
+
+    $('.third').click(()=>{
+        if($('#third').prop("checked") == true){
+            $('.three').removeClass('hide');
+        }
+        else if($('#third').prop("checked") == false){
+            $('.three').addClass('hide');
+        }
     })
 
