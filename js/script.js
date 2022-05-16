667e988cb099c01349141db3a589ab2ebee901b5
XODesigns <xolelanimdima@gmail.com>
2022-05-16 18:48:49 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 const backUs = document.querySelector(".back-us")
 const modalMain = document.querySelector(".modal")
+const pledgeSect = document.querySelectorAll("label")
+const visible = document.querySelectorAll("#visible")
 
-backUs.addEventListener("click", function(){
+backUs.addEventListener("click", ()=>{
     modalMain.classList.remove("visibility")
 })
 
 
-// $(".back-us").click(function(){
-//     $(".modal").removeClass("visibility")
-// })
+pledgeSect.forEach((e)=>{
+    e.addEventListener("click", ()=>{
+    
+    //    let check = e.checked = true
+
+    //    if(check){
+    //     pledge.classList.toggle("hide")
+    //    }
+
+  
+    
+
+        
+    })
+})
+
