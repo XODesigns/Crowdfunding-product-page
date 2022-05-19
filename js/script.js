fe3e0012132405b07c69006e4e0f125671734432
XODesigns <86083961+XODesigns@users.noreply.github.com>
2022-05-19 16:00:02 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,39 +1,64 @@
 const backUs = document.querySelector(".back-us")
 const modalMain = document.querySelector(".modal")
-const check = document.querySelectorAll("#checkbox")
+const label = document.querySelectorAll(".label")
+const pledge = document.querySelectorAll("#visible")
+const radio = document.querySelectorAll("input[type='radio']")
 
 backUs.addEventListener("click", ()=>{
     modalMain.classList.remove("visibility")
+    radio.forEach((e)=>{
+        e.checked=false
+    })
+    
 })
+
 
 
 /** Modal checkbox event in jquery -  to togglen visibility for the pledge section */
 
 
-    $('.first').click(()=>{
-        if($('#first').prop("checked") == true){
-            $('.one').removeClass('hide');
-        }
-        else if($('#first').prop("checked") == false){
-            $('.one').addClass('hide');
-        }
-    })
+    // $('.first').click(()=>{
+    //     if($('#first').prop("checked") == true){
+    //         $('.one').removeClass('hide');
+    //         $('.four').addClass('border')
+            
+    //     }
+    //     else if($('#first').prop("checked") == false){
+    //         $('.one').addClass('hide');
+    //     }
+    // })
 
-    $('.second').click(()=>{
-        if($('#second').prop("checked") == true){
-            $('.two').removeClass('hide');
-        }
-        else if($('#second').prop("checked") == false){
-            $('.two').addClass('hide');
-        }
-    })
+    // $('.second').click(()=>{
+    //     if($('#second').prop("checked") == true){
+    //         $('.two').removeClass('hide');
+    //         $('.five').addClass('border')
+    //     }
+    //     else if($('#second').prop("checked") == false){
+    //         $('.two').addClass('hide');
+    //     }
+    // })
 
-    $('.third').click(()=>{
-        if($('#third').prop("checked") == true){
-            $('.three').removeClass('hide');
+    // $('.third').click(()=>{
+    //     if($('#third').prop("checked") == true){
+    //         $('.three').removeClass('hide');
+    //         $('.five').addClass('border')
+    //     }
+    //     else if($('#third').prop("checked") == false){
+    //         $('.three').addClass('hide');
+    //     }
+    // })
+    
+    label.forEach((e)=>{
+    e.addEventListener("click", ()=>{
+        if(e.classList.contains("one")){
+            pledge[0].classList.toggle('hide')
+            radio[0].checked=true
+        } else if(e.classList.contains("two")){
+            pledge[1].classList.toggle('hide')
+            radio[1].checked=true
+        } else if(e.classList.contains("three")){
+            pledge[2].classList.toggle('hide')
+            radio[2].checked=true
         }
-        else if($('#third').prop("checked") == false){
-            $('.three').addClass('hide');
-        }
+        })
     })
-
