1ed5ab1551c882a68ac58bb258484c3bed83c60d
XODesigns <86083961+XODesigns@users.noreply.github.com>
2022-05-19 21:10:41 +0200

    update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const modalMain = document.querySelector(".modal")
 const label = document.querySelectorAll(".label")
 const pledge = document.querySelectorAll("#visible")
 const radio = document.querySelectorAll("input[type='radio']")
+const proceed = document.querySelectorAll(".continue")
+const thanks = document.querySelector(".thanks")
 
 backUs.addEventListener("click", ()=>{
     modalMain.classList.remove("visibility")
@@ -14,51 +16,36 @@ backUs.addEventListener("click", ()=>{
 
 
 
-/** Modal checkbox event in jquery -  to togglen visibility for the pledge section */
-
-
-    // $('.first').click(()=>{
-    //     if($('#first').prop("checked") == true){
-    //         $('.one').removeClass('hide');
-    //         $('.four').addClass('border')
-            
-    //     }
-    //     else if($('#first').prop("checked") == false){
-    //         $('.one').addClass('hide');
-    //     }
-    // })
-
-    // $('.second').click(()=>{
-    //     if($('#second').prop("checked") == true){
-    //         $('.two').removeClass('hide');
-    //         $('.five').addClass('border')
-    //     }
-    //     else if($('#second').prop("checked") == false){
-    //         $('.two').addClass('hide');
-    //     }
-    // })
-
-    // $('.third').click(()=>{
-    //     if($('#third').prop("checked") == true){
-    //         $('.three').removeClass('hide');
-    //         $('.five').addClass('border')
-    //     }
-    //     else if($('#third').prop("checked") == false){
-    //         $('.three').addClass('hide');
-    //     }
-    // })
-    
+/** Modal checkbox events -  to togglen visibility for the pledge section */
+ 
     label.forEach((e)=>{
     e.addEventListener("click", ()=>{
         if(e.classList.contains("one")){
             pledge[0].classList.toggle('hide')
             radio[0].checked=true
+            pledge[1].classList.add('hide')
+            pledge[2].classList.add('hide')
         } else if(e.classList.contains("two")){
             pledge[1].classList.toggle('hide')
             radio[1].checked=true
+            pledge[0].classList.add('hide')
+            pledge[2].classList.add('hide')
+
         } else if(e.classList.contains("three")){
             pledge[2].classList.toggle('hide')
             radio[2].checked=true
+            pledge[1].classList.add('hide')
+            pledge[0].classList.add('hide')
+
         }
         })
     })
+
+    /** continue buttons on the modal */
+proceed.forEach((e)=>{
+    e.addEventListener("click", (evt)=>{
+       evt.preventDefault()
+       modalMain.classList.add("visibility")
+        thanks.classList.remove('hide')
+    })
+})
