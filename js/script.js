48a56479a815eed16a800c474d99c27569900422
XODesigns <xolelanimdima@gmail.com>
2022-05-30 21:13:32 +0200

    update

--- js/script.js
+++ js/script.js
@@ -9,10 +9,15 @@ const amount = document.querySelectorAll("input[type='number']");
 const form = document.querySelector("#form");
 const fund = document.querySelector(".amount-backed");
 const btn = document.querySelector(".got-it");
-const rewardBtn = document.querySelectorAll(".reward")
+const rewardBtn = document.querySelector(".reward")
+const rewardBtnTwo = document.querySelector(".reward2")
 const pledger = document.querySelectorAll(".pledger")
 const pledgerSec = document.querySelectorAll(".pledgerSec")
 const bookmark = document.querySelector(".bookmark")
+const progressBar = document.querySelector(".bar")
+const backers = document.querySelector(".total-backers")
+
+
 
 backUs.addEventListener("click", () => {
   modalMain.classList.remove("visibility");
@@ -25,21 +30,34 @@ btn.addEventListener("click", () => {
   thanks.classList.add("hide");
 });
 
-rewardBtn.forEach((e) => {
-  e.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    modalMain.classList.remove("visibility");
 
-    if (rewardBtn[0]) {
-      pledge[1].classList.remove("hide")
-    } else if (rewardBtn[1]) {
-      pledge[2].classList.remove("hide")
-    }
-  })
+rewardBtn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  modalMain.classList.remove("visibility");
+
+  if (rewardBtn) {
+    pledge[1].classList.remove("hide")
+  }
+})
+
+rewardBtnTwo.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  modalMain.classList.remove("visibility");
+
+  if (rewardBtnTwo) {
+    pledge[2].classList.remove("hide")
+  }
 })
 
 
+/**Total Backers */
+let number = 5007;
+let nf = new Intl.NumberFormat('en-US');
+let backersLeft = nf.format(number);
 
+backers.innerHTML = `${backersLeft}`
+
+/**Backing Amount */
 let funding = 86914
 // Create our number formatter.
 let formatter = new Intl.NumberFormat('en-US', {
@@ -56,12 +74,17 @@ fund.innerHTML = `${funds}`
 /** Declare and place pledgers amount on the page */
 let firstCount = 101
 let secCount = 64
-pledger[0].innerHTML = `${firstCount} left`
-pledger[1].innerHTML = `${secCount} left`
-pledgerSec[0].innerHTML = `${firstCount} <span>left</span>`
-pledgerSec[1].innerHTML = `${secCount} <span>left</span>`
+let left = "left"
+pledger[0].innerHTML = `${firstCount} ${left}`
+pledger[1].innerHTML = `${secCount} ${left}`
+pledgerSec[0].innerHTML = `${firstCount} <span>${left}</span>`
+pledgerSec[1].innerHTML = `${secCount} <span>${left}</span>`
 
 
+/** Progress Bar placeholder laoder */
+
+let placeHolder = funding / 1000
+progressBar.style.width = `${placeHolder}%`
 
 /** Modal checkbox events -  to togglen visibility for the pledge section */
 
@@ -104,23 +127,12 @@ form.addEventListener("submit", (evt) => {
 
   /**update the pledges left */
 
-  if (amount[0]) {
-    let x = firstCount - 1
-    console.log(x)
-    pledger[0].innerHTML = `${x} left`
-    pledgerSec[0].innerHTML = `${x} left`
-  } else if (amount[1]) {
-    let y = secCount - 1
-    pledger[1].innerHTML = `${y} left`
-    pledgerSec[1].innerHTML = `${x} left`
-  }
-
+  backed()
 
 
   //   (e.value)
   // secCount++
-
-
+  totalBackers()
 
   amount.forEach((e) => {
     if (e.value) {
@@ -130,10 +142,16 @@ form.addEventListener("submit", (evt) => {
       arr.push(e.value);
       let sum = arr.reduce((a, b) => a + Number(b));
       console.log(sum);
-      // let count = 101
 
-      //   let result = e.value;
-      fund.innerHTML = `$ ${formatter.format(sum)}`;
+      /**progress bar */
+      let progress = e.value / 1000
+      let bar = placeHolder + progress
+
+      progressBar.style.width = `${bar}%`
+      console.log(`${bar}%`)
+
+
+      fund.innerHTML = ` ${formatter.format(sum)}`;
       modalMain.classList.add("visibility");
       thanks.classList.remove("hide");
       e.value = "";
@@ -149,10 +167,10 @@ form.addEventListener("submit", (evt) => {
 
 
 
-
-
-
-/**function that checks if the amount entered into the inputs equals to the starting amount */
+/**function that checks if the amount entered into the inputs equals to the starting amount 
+ * The second function is used to update the number of backers left
+ * the third is to update the total backers 
+ */
 
 function bamboo() {
 
@@ -163,4 +181,27 @@ function bamboo() {
   } else {
     e.value = "";
   }
+}
+
+function backed() {
+  if (amount[0]) {
+    let x = firstCount - 1
+    console.log(x)
+    pledger[0].innerHTML = `${x} left`
+    pledgerSec[0].innerHTML = `${x} left`
+  } else if (amount[1]) {
+    let y = secCount - 1
+    pledger[1].innerHTML = `${y} left`
+    pledgerSec[1].innerHTML = `${x} left`
+  }
+
+}
+
+function totalBackers() {
+  if (amount) {
+    let x = number + 1
+    backers.innerHTML = `${nf.format(x)}`
+  }
+
+
 }
